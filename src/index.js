fc7868e7a417f73886bfb591ba3e3e55c0aa6ece
Brun0-marinh0 <bcaldasmarinhojklb@gmail.com>
2022-08-07 19:55:06 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const {v4: uuidv4} = require('uuid')
+
+const app = express()
+app.use(express.json())
+
+const customers = []
+
+app.post("/account", (request, response) => {
+    const {cpf, name} = request.body
+
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    )
+
+    if(customerAlreadyExists){
+        return response.status(400).json({error: "Customer already Exists!"})
+    }
+
+    const id = uuidv4()
+
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement:[]
+    })
+
+    return response.status(201).send()
+})
+
+app.get("/statement/:cpf",(request, response) => {
+    const {cpf} = request.headers
+
+    const customer = customers.find(customer => customer.cpf === cpf)
+
+    if(!customer){
+        return response.status(400).json({error: "Customer not found"})
+    }
+
+    return response.json(customer.statement)
+})
+app.listen(3333)
