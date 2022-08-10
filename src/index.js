da997bfcd97e9887344c33180157b085cf8d2383
Brun0-marinh0 <bcaldasmarinhojklb@gmail.com>
2022-08-09 21:10:52 -0400

    middleware

--- src/index.js
+++ src/index.js
@@ -5,6 +5,20 @@ const app = express()
 app.use(express.json())
 
 const customers = []
+// middleware
+function verifyIfExistsAccountCPF(request, response, next){
+    const {cpf} = request.headers
+
+    const customer = customers.find(customer => customer.cpf === cpf)
+
+    if(!customer){
+        return response.status(400).json({error: "Customer not found"})
+    }
+
+    request.customer = customer
+    
+    return next()
+}
 
 app.post("/account", (request, response) => {
     const {cpf, name} = request.body
@@ -29,15 +43,7 @@ app.post("/account", (request, response) => {
     return response.status(201).send()
 })
 
-app.get("/statement/:cpf",(request, response) => {
-    const {cpf} = request.headers
-
-    const customer = customers.find(customer => customer.cpf === cpf)
-
-    if(!customer){
-        return response.status(400).json({error: "Customer not found"})
-    }
-
+app.get("/statement/:cpf", verifyIfExistsAccountCPF, (request, response) => {
     return response.json(customer.statement)
 })
 app.listen(3333)
